e5a519e69b8e4fba08cc13fdf743521e777f2b3f
Sander van Doorn <s.van.doorn@st.hanze.nl>
2018-11-06 12:08:18 +0100

    Added timeout to communication test function and start of object

--- frontend/communicator.py
+++ frontend/communicator.py
@@ -1,11 +1,13 @@
 import serial
+import serial.tools.list_ports
 from time import sleep
 
 
-def sendCommand(command, port):
+def send_command(command, port):
     connection = serial.Serial(
         port=port,
         baudrate=9600,
+        timeout=5,
     )
 
     if connection.isOpen():
@@ -17,20 +19,39 @@ def sendCommand(command, port):
     sleep(2)
 
     if connection.inWaiting() == 0:
+        print("Command: " + command)
         line = command + "\r\n"
         connection.write(line.encode())
 
-    endchar = '>'
-    inchar = ''
-    response = ''
-    while endchar is not inchar:
-        inchar = connection.read().decode()
+    response = connection.readline()
+    print("Response: " + response.decode('utf-8'))
 
-        if endchar is not inchar:
-            response += inchar
-
-    # Trim the original command of response
-    return response.split('\n')[1]
+    # Return the response given by Arduino
+    return response.decode()
 
 
-print(sendCommand("1", 'COM3'))
+def check_ports():
+    """
+    Returns a list of COM devices identified as zeng screen
+    """
+
+    comlist = serial.tools.list_ports.comports()    # All connected COM ports
+    connected = []                                  # All connected COM ports by ID
+    active_screens = []                             # All active COM ports identified as zeng screen
+    for devices in comlist:
+        # Add device to active_screens if handshake succeeds
+        if devices.device not in active_screens:
+            if send_command("WHO_ARE_YOU", devices.device) == "I AM GROOT":
+                active_screens.append(devices.device)
+
+        # Check if devices have been removed
+        # By comparing active_list to all connected devices
+        connected.append(devices.device)
+        for screen in active_screens:
+            if screen not in connected:
+                active_screens.remove(screen)
+
+    return active_screens
+
+
+send_command("SET_THOLD_DIST 20", 'COM3')
